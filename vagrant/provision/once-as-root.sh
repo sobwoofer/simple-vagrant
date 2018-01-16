ac4accc786d93e2a354e5f553a8bc4b5f63c883b
volynets <volynets@papirus.com.ua>
2018-01-16 11:36:43 +0200

    fix readme, added stack

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -41,7 +41,7 @@ apt-get update
 apt-get upgrade -y
 
 info "Install additional software"
-apt-get install -y php7.1-curl php7.1-cli php7.1-intl php7.1-mysqlnd php7.1-gd php7.1-fpm php7.1-mbstring php7.1-xml php-xdebug unzip nginx mysql-server-5.7 mc php7.1-zip php-mcrypt php7.1-memcached php7.1-soap memcached npm
+apt-get install -y php7.1-curl php7.1-cli php7.1-intl php7.1-mysqlnd php7.1-gd php7.1-fpm php7.1-mbstring php7.1-xml php7.1-xdebug unzip nginx mysql-server-5.7 mc php7.1-zip php-mcrypt7.1 php7.1-memcached php7.1-soap memcached npm
 
 info "Install Oracle JDK"
 debconf-set-selections <<< "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true"
